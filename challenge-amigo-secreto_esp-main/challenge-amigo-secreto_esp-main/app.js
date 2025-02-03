7cea14ff4af63286251a264c3fa94326e9aea1c0
SpearShadow <jerecrc1@gmail.com>
2025-02-02 22:13:35 -0400

    Create fuction agregarAmigo y Create function listarAmigos

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -2,3 +2,30 @@
 
 
 let amigos = [];
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre.");
+    }
+    else {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = "";
+        listarAmigos();
+    }
+    
+}
+
+
+function listarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos"); // Obtener el elemento de la lista
+    listaAmigos.innerHTML = ""; // Limpiar la lista existente
+
+    for (let i = 0; i < amigos.length; i++) { // Iterar sobre el arreglo
+        let li = document.createElement("li"); // Crear un elemento <li>
+        li.textContent = amigos[i]; // Agregar el nombre del amigo
+        listaAmigos.appendChild(li); // Agregar <li> a la lista
+    }
+
+}
